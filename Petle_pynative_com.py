cb70cb064c86a9e4b98e56e531b9abb1346f528c
no-logo <andrzej.wawryk@gmail.com>
2024-09-27 15:01:54 +0200

    Exercise 3,4,5

--- Petle_pynative_com.py
+++ Petle_pynative_com.py
@@ -14,3 +14,30 @@ while i <= 5:
         print(j, end=' ')
     print('')
     i += 1
+
+#Exercise 3: Calculate sum of all numbers from 1 to a given number
+print('\nExercise 3\n')
+
+a = int(input('Podaj liczbę a: '))
+suma = 0
+for s in range(1, a+1):
+    suma += s
+print(f'suma liczb od 1 do a = {suma}')
+
+#Exercise 4: Print multiplication table of a given number
+print('\nExercise 4\n')
+
+a = int(input('Podaj liczbę a: '))
+for r in range(1, 11):
+    print(a*r)
+
+#Exercise 5: Display numbers from a list using a loop
+print('\nExercise 5\n')
+
+numbers = [12, 75, 150, 180, 145, 525, 50]
+
+for n in numbers:
+    if n > 500:
+        break
+    if n % 5 ==0 and n <= 150:
+        print(n)
