1ad9e82f1a9476ab9dc08316e5131b8b718dffb9
no-logo <andrzej.wawryk@gmail.com>
2024-10-08 16:17:04 +0200

    Task 1 - 3

--- Module_Functions_Part_1.py
+++ Module_Functions_Part_1.py
@@ -1,4 +1,6 @@
 # Zadanie 1: def print_sentence_multiple_times(): # output: (input = 3) # Hello, Python! # Hello, Python! # Hello, Python!
+from scipy.fft import ifftshift
+
 print('\nZadanie 1\n')
 def print_sentence_multiple_times(t):
     sentence = '# Hello, Python! '
@@ -35,4 +37,60 @@ def shortest_longest_words(words_list):
 
 sorted_list = shortest_longest_words(['grapefruit','kiwi','mango'])
 print(sorted_list)
+
+#Task 1
+print('\nTask 1\n')
+
+def print_formated_text():
+    print('"Don\'t compare yourself with anyone in this world…\nif you do so, you are insulting yourself."\n\t\t\t\t\t\t\t\t\t\t\tBill Gates')
+
+print_formated_text()
+
+#Task 2
+print('\nTask 2\n')
+
+def even_numbers(a, b):
+    if a % 2 == 0:
+        a1 = a
+    else:
+        a1 = a + 1
+
+    if b % 2 == 0:
+        b1 = b + 1
+    else:
+        b1 = b
+
+    en = list(range(a1, b1,2))
+    print(en)
+
+even_numbers(2,9)
+
+#Task 3
+print('\nTask 3\n')
+
+def print_square(length, symbol, isSolid):
+    if isSolid:
+        fsymbol = symbol
+    else:
+        fsymbol = ' '
+
+    for n in range(length):
+        if length == 1:
+            print(symbol, end='')
+            break
+        if length == 2:
+            print(symbol*2, end='')
+        else:
+            if n == 0 or n == length - 1:
+                print(symbol*length, end='')
+            else:
+                print(symbol, end='')
+                print(fsymbol*(length-2), end='')
+                print(symbol, end='')
+        print()
+
+print_square(10,'*',False)
+print('\n')
+print_square(10,'*',True)
+
 
