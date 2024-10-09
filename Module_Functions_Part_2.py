8f7e3f9ca28f8db5b380df11f0557eaec9e27baa
no-logo <andrzej.wawryk@gmail.com>
2024-10-09 17:24:58 +0200

    task 1 - 3

--- Module_Functions_Part_2.py
+++ Module_Functions_Part_2.py
@@ -21,3 +21,44 @@ print(remove_duplicates_and_sort(['orange', 'kiwi', 'banana', 'apple', 'apple', 
 
 #Task 1
 print('\nTask 1\n')
+
+def product_of_elements(input_list):
+    p = 1
+    for element in input_list:
+        p *= element
+    return p
+
+print(product_of_elements([1, 2, 3, 4]))
+
+#Task 2
+print('\nTask 2\n')
+
+def minimum_in_list(input_list):
+    list_min = input_list[0]
+    for i in range(1, len(input_list)):
+        if input_list[i] < list_min:
+            list_min = input_list[i]
+    return list_min
+
+print(minimum_in_list([1, 2, 3, 4, 0]))
+
+#task 3
+print('\nTask 3\n')
+
+def check_prime_numbers(input_list):
+    prime_count = 0
+    isprime = 1
+    for i in input_list:
+        if i == 1 or i == 2:
+            prime_count += 1
+        else:
+            for j in range(2, i):
+                if i % j == 0:
+                    isprime = 0
+            prime_count += isprime
+        isprime = 1
+
+    return prime_count
+
+print(check_prime_numbers([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
