ca770edeaa3c1bb09c1a5eeb2bc375de82280303
no-logo <andrzej.wawryk@gmail.com>
2024-10-14 13:44:55 +0200

    task 2

--- Module_Functions_Part_3.py
+++ Module_Functions_Part_3.py
@@ -30,3 +30,29 @@ def find_greatest_common_divisor(a, b, d = 0):
 
 print(find_greatest_common_divisor(8,12))
 
+# Task 2
+print('\nTask 2\n')
+
+import random
+def bulls_and_cows(n, a = 1):
+    g = input('Guess four-digit number: ')
+
+    if g == n:
+        return a
+    else:
+        bools = 0
+        cows = 0
+        for i in range(4):
+            if g[i] in n:
+                bools += 1
+            if g[i] == n[i]:
+                cows += 1
+        print(f'bools = {bools}, cows = {cows}')
+        a += 1
+        return bulls_and_cows(n, a)
+
+
+numbers = (random.sample(range(10), 4))
+number = ''.join([str(x) for x in numbers])
+print(bulls_and_cows(number))
+print(f'my number was: {number}')
