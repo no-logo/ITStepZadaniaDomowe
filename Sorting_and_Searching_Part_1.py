190e8492fba66f73c36d77631e70fe608d8a907e
no-logo <andrzej.wawryk@gmail.com>
2024-10-20 20:33:53 +0200

    2. Selection sort

--- Sorting_and_Searching_Part_1.py
+++ Sorting_and_Searching_Part_1.py
@@ -41,3 +41,27 @@ rand_list = [random.randint(0,100) for _ in range(20)]
 print(rand_list)
 sorted_rand_list = selection_sort(rand_list)
 print(sorted_rand_list)
+
+#3. Insertion sort
+print('\n3. Insertion sort\n')
+
+def insertion_sort(slist):
+    j = 0
+    while j < len(slist)-1:
+        elem_to_insert = slist[j+1]
+        for i in range(j, -1, -1):
+            if slist[i] > elem_to_insert:
+                slist[i+1] = slist[i]
+                if i == 0:
+                    slist[0] = elem_to_insert
+            else:
+                slist[i+1] = elem_to_insert
+                break
+        j += 1
+    return slist
+
+rand_list = [random.randint(0,100) for _ in range(20)]
+
+print(rand_list)
+sorted_rand_list = insertion_sort(rand_list)
+print(sorted_rand_list)
