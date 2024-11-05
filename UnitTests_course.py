bb6ec46e7dfeae8424c654660d33b4ca0b1abffd
no-logo <andrzej.wawryk@gmail.com>
2024-11-05 16:26:57 +0100

    unit tests

--- UnitTests_course.py
+++ UnitTests_course.py
@@ -15,6 +15,13 @@ class TestCalculateAverage(unittest.TestCase):
         self.assertEqual(calculate_average([2.5, 3.5, 4.5]), 3.5, 'test 2')
         self.assertEqual(calculate_average([0.5,1.5]), 1, 'test 3')
 
+    def test_calculate_average_empty_input(self):
+        self.assertEqual(calculate_average([]), None, 'test 1')
+
+    def test_calculate_average_invalid_input(self):
+        self.assertRaises(TypeError, calculate_average, ['a', 3])
+        self.assertRaises(TypeError, calculate_average, [1, 2, '2'])
+        self.assertRaises(TypeError, calculate_average, [None, None])
 if __name__ == '__main__':
     unittest.main()
 
