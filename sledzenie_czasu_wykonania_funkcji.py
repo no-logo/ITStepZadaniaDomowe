9daa9342f13f0307deb445f2830c5bd868247b58
no-logo <andrzej.wawryk@gmail.com>
2024-11-07 08:08:36 +0100

    miernik

--- sledzenie_czasu_wykonania_funkcji.py
+++ sledzenie_czasu_wykonania_funkcji.py
@@ -7,15 +7,15 @@ def fun_wait():
     time.sleep(i)
 
 
-def miernik_wykonania(f):
+def miernik_wykonania(fun):
     licznik = 0
 
     def zmierz_czas():
         nonlocal licznik
         licznik += 1
-        t = time.time()
-        f()
-        return [time.time() - t, licznik]
+        t_start = time.time()
+        fun()
+        return [time.time() - t_start, licznik]
 
     return zmierz_czas
 
@@ -24,5 +24,5 @@ miernik = miernik_wykonania(fun_wait)
 
 for i in range(5):
     m = miernik()
-    print(f'Czas wykonania funkcji: {m[0]} ilość pomiarów: {m[1]}')
+    print(f'Czas wykonania funkcji: {m[0]} sec., ilość pomiarów: {m[1]}')
 
