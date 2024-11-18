27f6d4796dad632513788ec2a9c7302178f9dddd
no-logo <andrzej.wawryk@gmial.com>
2024-11-18 19:35:46 +0100

    unit converter

--- Przeciazanie_operatorow_5.py
+++ Przeciazanie_operatorow_5.py
@@ -1,5 +1,5 @@
-from lief import not_implemented
 
+#Zadanie 1
 
 class Circle:
     def __init__(self, r):
@@ -59,3 +59,125 @@ print(c1>=c2)
 print(c1>c2)
 print(c1<c3)
 print(c1<=c3)
+
+#Zadanie 2
+
+class Complex:
+    def __init__(self, real, imag):
+        self.__r = real
+        self.__i = imag
+
+    def __str__(self):
+        return f'cx = {self.__r} + {self.__i}i'
+
+    def __add__(self, other):
+        if isinstance(other, Complex):
+            return Complex(self.__r + other.__r, self.__i + other.__i)
+        return NotImplemented
+
+    def __sub__(self, other):
+        if isinstance(other, Complex):
+            return Complex(self.__r - other.__r, self.__i - other.__i)
+        return NotImplemented
+
+    def __mul__(self, other):
+        if isinstance(other, Complex):
+            return Complex(self.__r * other.__r, self.__i * other.__i)
+        return NotImplemented
+
+    def __truediv__(self, other):
+        if isinstance(other, Complex):
+            return Complex(self.__r / other.__r, self.__i * other.__i)
+        return NotImplemented
+
+cx1 = Complex(6, 4)
+cx2 = Complex(3, 2)
+
+print(cx1 + cx2)
+print(cx1 - cx2)
+print(cx1 * cx2)
+print(cx1 / cx2)
+
+#Zadanie 3
+
+class Plane:
+    def __init__(self, nop):
+        self.__number_of_passengers = nop
+
+    def __add__(self, other):
+        if isinstance(other, Plane):
+            return Plane(self.__number_of_passengers + other.__number_of_passengers)
+        else:
+            return NotImplemented
+
+    def __sub__(self, other):
+        if isinstance(other, Plane):
+            return Plane(self.__number_of_passengers - other.__number_of_passengers)
+        else:
+            return NotImplemented
+
+    def __iadd__(self, other):
+        if isinstance(other, Plane):
+            self.__number_of_passengers += other.__number_of_passengers
+            return self
+        else:
+            return NotImplemented
+
+    def __isub__(self, other):
+        if isinstance(other, Plane):
+            self.__number_of_passengers -= other.__number_of_passengers
+            return self
+        else:
+            return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, Plane):
+            return Plane(self.__number_of_passengers > other.__number_of_passengers)
+        else:
+            return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other, Plane):
+            return Plane(self.__number_of_passengers >= other.__number_of_passengers)
+        else:
+            return NotImplemented
+
+    def __lt__(self, other):
+        if isinstance(other, Plane):
+            return Plane(self.__number_of_passengers < other.__number_of_passengers)
+        else:
+            return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, Plane):
+            return Plane(self.__number_of_passengers <= other.__number_of_passengers)
+        else:
+            return NotImplemented
+
+#Zadanie 4
+
+class Apartment:
+    def __init__(self, area, price):
+        self.__area = area
+        self.__price = price
+
+    def __eq__(self, other):
+        if isinstance(other, Apartment):
+            return self.__area == other.__area
+        else:
+            return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other, Apartment):
+            return self.__area != other.__area
+        else:
+            return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, Apartment):
+            return self.__price > other.__price
+        else:
+            return NotImplemented
+
+
+
