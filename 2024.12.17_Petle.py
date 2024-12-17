5aa4cd3a54c888b2566677dd58acff0dc29fbd61
no-logo <andrzej.wawryk@gmial.com>
2024-12-17 19:45:06 +0100

    powtorka petle 2

--- 2024.12.17_Petle.py
+++ 2024.12.17_Petle.py
@@ -43,3 +43,42 @@ while liczba1 <= liczba2:
     if liczba1 % 2 == 1:
         print(liczba1)
     liczba1 += 1
+
+#Zadanie 1
+liczba1 = int(input('Podaj pierwszą liczbę: '))
+liczba2 = int(input('Podaj drugą liczbę: '))
+
+if liczba2 < liczba1:
+    liczba2, liczba1 = liczba1, liczba2
+
+suma_liczb = 0
+
+while liczba1 <= liczba2:
+    suma_liczb += liczba1
+    liczba1 += 1
+
+srednia_liczb = suma_liczb / (liczba2 - liczba1 +1)
+
+print(f'Suma liczb to {srednia_liczb}')
+print(f'Średnia liczb to {srednia_liczb}')
+
+#Zadanie 2
+liczba1 = int(input('Podaj pierwszą liczbę: '))
+
+iloczyn = 1
+
+while 0 < liczba1:
+    iloczyn *= liczba1
+    liczba1 -= 1
+
+print(f'{liczba1}! = {iloczyn}')
+
+#Zadanie 3
+dl_linii = int(input('Podaj dlugosc linii: '))
+print('*'*dl_linii)
+
+#Zadanie 4
+dl_linii = int(input('Podaj dlugosc linii: '))
+symbol = input('Podaj symbol: ')
+print(symbol*dl_linii)
+
